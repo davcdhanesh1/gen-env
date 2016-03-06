821b09dd9c3b6ec88a6b356e71a67d46b0d2c159
davcdhanesh1 <davcdhanesh1@gmail.com>
2016-03-06 10:48:34 +0530

    Dhanesh - Bump up version to 0.2.2

--- lib/gen/env/version.rb
+++ lib/gen/env/version.rb
@@ -1,5 +1,5 @@
 module Gen
   module Env
-    VERSION = "0.2.1"
+    VERSION = "0.2.2"
   end
 end
--- spec/gen/env_spec.rb
+++ spec/gen/env_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Gen::Env do
   it 'has a version number' do
-    expect(Gen::Env::VERSION).to eq "0.2.1"
+    expect(Gen::Env::VERSION).to eq "0.2.2"
   end
 end
